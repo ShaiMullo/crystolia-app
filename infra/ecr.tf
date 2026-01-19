bbc04e62a1c6bce0c91d5275753c057749433dcc
shaimullo <shaimullokandov@gmail.com>
2026-01-19 16:43:06 +0200

    fix: add force_delete to ECR repos, cleanup Route53 records for terraform destroy

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "frontend" {
   name                 = "crystolia-frontend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -13,6 +14,7 @@ resource "aws_ecr_repository" "frontend" {
 resource "aws_ecr_repository" "backend" {
   name                 = "crystolia-backend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
